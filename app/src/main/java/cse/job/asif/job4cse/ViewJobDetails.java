5e9d1bd8a0bc7c5844133adec488162345eeffc0
Asifur Rahman <mail11asifur@gmail.com>
2018-11-13 07:07:57 +0600

    Added ViewJobDetails activity

---
+++ app/src/main/java/cse/job/asif/job4cse/ViewJobDetails.java
@@ -0,0 +1,13 @@
+package cse.job.asif.job4cse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewJobDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_job_details);
+    }
+}
